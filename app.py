bc1c40fd6a9bc58ef9ce56865cbf57b0b67cd80e
Sandra <sandrallancherosg@gmail.com>
2024-02-04 18:30:53 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Request
+from logging import Logger
+import os
+
+from slack_bolt import App
+from slack_bolt.adapter.fastapi import SlackRequestHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+slack_token = os.environ.get("SLACK_BOT_TOKEN")
+logger = Logger("flash_bot")
+
+api = FastAPI()
+slack_app = App(token=slack_token)
+app_handler = SlackRequestHandler(slack_app)
+
+@api.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@slack_app.command("/llm_bot")
+def llm(ack, say):
+    ack("Received /llm command.")
+    say("Hello, LLM!")
+
+@slack_app.event("app_mention")
+def handle_app_mentions(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
+
+@api.post("/slack/events")
+async def endpoint(req: Request):
+    logger.log("I got here")
+    return await app_handler.handle(req)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(api, host="0.0.0.0", port=3000)
